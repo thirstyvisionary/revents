f8c3b28f5de0db89ef21182192acfae68ce4c487
carrot <thirstyvisionary@gmail.com>
2019-11-11 23:22:43 +0000

    End of section 13

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -1,46 +1,68 @@
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT, FETCH_EVENTS } from "./eventConstants";
-import { asyncActionStart, asyncActionFinish, asyncActionError } from "../async/asyncActions";
-import { fetchSampleData } from "../../app/data/mockApi";
+import {
+  CREATE_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT,
+  FETCH_EVENTS
+} from './eventConstants';
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError
+} from '../async/asyncActions';
+import { fetchSampleData } from '../../app/data/mockApi';
+import { toastr } from 'react-redux-toastr';
 
-export const createEvent = (event) => {
-    return {
-        type: CREATE_EVENT,
-        payload: {
-            event
-        }
-    }
-}
-
-export const updateEvent = (event) => {
-    return {
-        type: UPDATE_EVENT,
-        payload: {
-            event
-        }
-    }
-}
-
-export const deleteEvent = (eventId) => {
-    return {
-        type: DELETE_EVENT,
-        payload: {
-            eventId
-        }
-    }
-    
-}
-
-export const loadEvents = () => {
+export const createEvent = event => {
     return async dispatch => {
         try {
-            dispatch(asyncActionStart())
-            const events = await fetchSampleData();
-            dispatch({type: FETCH_EVENTS, payload: {events}})
-            dispatch(asyncActionFinish())
+          dispatch({
+            type: CREATE_EVENT,
+            payload: {
+              event
+            }
+          });
+          toastr.success('Success!', 'Event has been created');
         } catch (error) {
-            console.log(error);
-            dispatch(asyncActionError())
-
+          toastr.error('Oops', 'Something went wrong');
         }
+      };
+};
+
+export const updateEvent = event => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: UPDATE_EVENT,
+        payload: {
+          event
+        }
+      });
+      toastr.success('Success!', 'Event has been updated');
+    } catch (error) {
+      toastr.error('Oops', 'Something went wrong');
     }
-}
+  };
+};
+
+export const deleteEvent = eventId => {
+  return {
+    type: DELETE_EVENT,
+    payload: {
+      eventId
+    }
+  };
+};
+
+export const loadEvents = () => {
+  return async dispatch => {
+    try {
+      dispatch(asyncActionStart());
+      const events = await fetchSampleData();
+      dispatch({ type: FETCH_EVENTS, payload: { events } });
+      dispatch(asyncActionFinish());
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionError());
+    }
+  };
+};
