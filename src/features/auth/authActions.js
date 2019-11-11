bea4ff025a7f273b656505597ef0027627dd3538
carrot <thirstyvisionary@gmail.com>
2019-11-11 16:36:36 +0000

    End of section 11

---
+++ src/features/auth/authActions.js
@@ -0,0 +1,18 @@
+import { LOGIN_USER, SIGN_OUT_USER } from "./authConstants"
+
+export const login = (creds) => {
+    return {
+        type: LOGIN_USER,
+        payload: {
+            creds
+        }
+    }
+}
+
+export const logout = () => {
+    return {
+        type: SIGN_OUT_USER
+
+    }
+    
+}
