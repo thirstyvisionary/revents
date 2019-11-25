81a1a212e36e147ea46669efa3d34acbf1d33edd
carrot <thirstyvisionary@gmail.com>
2019-11-25 17:02:16 +0000

    End of section 21

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -95,10 +95,9 @@ export const getEventsForDashboard = lastEvent => async (
     let querySnap = await query.get();
 
     if (querySnap.docs.length === 0) {
-      dispatch(asyncActionFinish())
+      dispatch(asyncActionFinish());
       return querySnap;
     }
-
 
     let events = [];
 
@@ -114,3 +113,24 @@ export const getEventsForDashboard = lastEvent => async (
     dispatch(asyncActionError());
   }
 };
+
+export const addEventComment = (eventId, values, parentId) =>
+  async (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+    const profile = getState().firebase.profile;
+    const user = firebase.auth().currentUser;
+    let newComment = {
+      parentId: parentId,
+      displayName: profile.displayName,
+      photoURL: profile.photoURL || '/assets/user.png',
+      uid: user.uid,
+      text: values.comment,
+      date: Date.now()
+    }
+    try {
+      await firebase.push(`event_chat/${eventId}`, newComment);
+    } catch (error) {
+      console.log(error)
+      toastr.error('Oops', 'Problem adding comment')
+    }
+  }
